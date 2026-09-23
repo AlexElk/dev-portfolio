c53723a169c51da1b07e4a8d4bcbd34d4d84c688
AlexElk <shaztapinekone@gmail.com>
2026-09-23 11:40:58 -0600

    fix: Changed the basePath for  Github actios -The path inside next.config.ts might be a problem

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/dev-portfolio' : '',
+};
+
+export default nextConfig;
